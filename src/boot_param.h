62673676739801bbdb587e4f50a7acac7816d0e5
yzfcer <yzfcer@163.com>
2017-12-17 16:20:41 +0800

    代码整理

--- src/boot_param.h
+++ src/boot_param.h
@@ -36,8 +36,6 @@ typedef struct
     w_uint8_t run_type;   //运行在RAM或Flash
     w_uint8_t encrypt_type;//程序加密使能
     w_uint8_t lock_en;     //芯片锁定使能
-    //w_int8_t phymem_max;
-    //w_int8_t part_max;
     w_int8_t phymem_cnt;
     w_int8_t part_cnt;
     char runreg[PART_NAME_LEN];
